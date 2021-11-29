7956a5473e4855e6f19def09952a58f7e82056e1
nandgreat <paul4nank@gmail.com>
2021-11-29 10:38:24 +0100

    Text and using states

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('nandom');
+  const [person, setPerson] = useState({ name: 'Makario', age: 55 });
+
+  const clickHandler = () => {
+    setName("Nandom Kumchi");
+    setPerson({name: 'Efemena Alex', age: 82})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>My Brother's name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update State" onPress={clickHandler} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +27,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    color: '#ff00ff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 30,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
