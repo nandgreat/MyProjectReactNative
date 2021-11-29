0546f076b0e15bc92ba7b949eba45848bf6cdee9
nandgreat <paul4nank@gmail.com>
2021-11-29 11:45:36 +0100

    Flat List and Scrollview

--- App.js
+++ App.js
@@ -1,28 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('nandom');
-  const [age, setAge] = useState('36');
-  const [person, setPerson] = useState({ name: 'Makario', age: 55 });
 
-  const clickHandler = () => {
-    setName("Nandom Kumchi");
-    setPerson({ name: 'Efemena Alex', age: 82 })
-  }
+  const [people, setPeople] = useState([
+    { name: 'Sophia', id: '37' },
+    { name: 'Plangnan', id: '9' },
+    { name: 'Nandom', id: '65' },
+    { name: 'Angir', id: '24' },
+    { name: 'Fwangzeen', id: '12' },
+    { name: 'Pebang', id: '25' },
+    { name: 'Makario', id: '51' },
+    { name: 'Sophia', id: '31' },
+    { name: 'Plangnan', id: '79' },
+    { name: 'Nandom', id: '69' },
+    { name: 'Angir', id: '28' },
+    { name: 'Fwangzeen', id: '14' },
+    { name: 'Pebang', id: '20' },
+    { name: 'Makario', id: '55' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput multiline style={styles.input} placeholder="e.g John Doe"
-        onChangeText={(value) => setName(value)} />
 
-      <Text>Enter Age:</Text>
-      <TextInput keyboardType='numeric' style={styles.input} placeholder="e.g 45"
-        onChangeText={(value) => setAge(value)} />
-      <Text>name: {name}, age: {age}</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          (
+            <View style={styles.itemStyle}>
+              <Text style={styles.listStyle}>
+                {item.name}
+              </Text>
+            </View>
+          )
+        )}
+      />
+
+      {/* <ScrollView>
+        {people.map(item => {
+          return (
+            <View style={styles.itemStyle}>
+              <Text style={styles.listStyle}>
+                {item.name}
+              </Text>
+            </View>
+          )
+        })}
+      </ScrollView> */}
     </View>
   );
 }
@@ -31,29 +58,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 15.0,
     color: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    paddingHorizontal: 22
   },
-  header: {
+  listStyle: {
+    fontSize: 28.0,
+    color: 'red'
+  },
+  itemStyle: {
     backgroundColor: 'pink',
-    padding: 30,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20
-  },
-  buttonContainer: {
-    marginTop: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#aaa',
-    padding: 8,
     margin: 10,
-    width: 250
+    padding: 10
   }
 });
