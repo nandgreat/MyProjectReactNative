c841724ff9de92a4bf9186e60b237e6ca44f7ee0
nandgreat <paul4nank@gmail.com>
2021-12-02 14:36:36 +0100

    Using Modals

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import { Text, FlatList, View } from 'react-native';
+import { Text, FlatList, View, Modal, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Card from '../shared/card';
 import { globalStyles } from '../styles/global';
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Home({ navigation }) {
+
+    const [modalOpen, setModalOpen] = useState(false);
 
     const [reviews, setReviews] = useState([
         { title: "Zelda, Breath of Fresh Air", rating: 5, body: 'Lorem ipsum', key: '1' },
@@ -14,6 +17,27 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        style={[styles.modalToggle, styles.modalClose]}
+                        onPress={() => setModalOpen(false)}
+                    />
+
+                    <Text>Hello from modal</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList
                 data={reviews}
                 keyExtractor={(item) => item.key}
@@ -30,3 +54,21 @@ export default function Home({ navigation }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10.0,
+        borderWidth: 1,
+        borderColor: '#dddddd',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center'
+    },
+    modalClose: {
+        marginTop: 20.0,
+        marginBottom: 0,
+    },
+    modalContent: {
+        flex: 1
+    }
+});
